4116eecf297ac10dca8a4395d2a3893cbafca3d4
yoochanghwan <marugo23@gmail.com>
2022-11-29 19:23:22 +0900

    create

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Button1 from './components/Button1';
+import Button2 from './components/Button2';
+import StyledComponent from './StyledComponent';
+import TailwindComponent from './TailwindComponent';
+
+function App() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+      <StyledComponent />
+      <TailwindComponent />
+    </>
+  );
+}
+
+export default App;
